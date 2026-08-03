44d9d6057437e7923ba511e6df3d355d799dffaf
arslan <arslan.dev51@gmail.com>
2026-08-04 00:20:29 +0500

    push this code

--- backend/expense/views.py
+++ backend/expense/views.py
@@ -4,6 +4,8 @@ import json
 from .models import *
 
 # Create your views here.
+
+# Signup API
 @csrf_exempt
 def signup(request):
   if request.method == 'POST':
@@ -16,3 +18,17 @@ def signup(request):
       return JsonResponse({'message':'Email already exists'},status=400)
     UserDetail.objects.create(FullName=fullname,Email=email,Password=password)
     return JsonResponse({'message':'User registered successfully'},status=201)
+
+# Login API
+@csrf_exempt
+def login(request):
+  if request.method == 'POST':
+    data = json.load(request.body)
+    email = data.get('Email')
+    password = data.get('Password')
+
+    try:
+      user = UserDetail.objects.get(Email=email,Password=password)
+      return JsonResponse({'message':'Login Successfully','userId':user.id,'userName':user.FullName},status=200)
+    except:
+      return JsonResponse({'message':'Invalid credentials'},status=400)
